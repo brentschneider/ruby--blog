78027d049717f991fd009f8622fbdbc04b390ed2
Brent Schneider <brent@milkshakeinteractive.com>
2014-09-22 22:06:17 -0700

    adding to git hub

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
